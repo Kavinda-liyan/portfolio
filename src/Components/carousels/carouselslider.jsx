1bd466e84be4e671b52a32ab5bff6cd710ae54d5
Kavinda-liyan <110520736+Kavinda-liyan@users.noreply.github.com>
2024-05-09 09:53:02 +0530

    Project page commit2

--- src/Components/carousels/carouselslider.jsx
+++ src/Components/carousels/carouselslider.jsx
@@ -1,55 +1,25 @@
 import { Container } from "react-bootstrap";
-import { Carousel } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 
 function CarouselSlider(props){
     return(
-        <Container>
-          <h5>{props.Caption}</h5>
-          <p><Link to={props.project} > Project Link</Link></p>
-            <Carousel data-bs-theme="dark">
-      <Carousel.Item>
-      
-          
-      
-        <img
-          className="d-block w-100 "
+        <Container >
+         <Link to={props.project} target="_blank" className="projectlinks"> <div className="ProjectCard">
+           <img
+          className="d-block w-100 projectImages"
           src={props.img1}
           alt={props.alt1}
-          height="250px"
+          height="150px"
         />
-        <p>{props.dis1}</p>
+        <h5><FontAwesomeIcon icon={props.Picon}/>{props.Caption}</h5>
         
-      </Carousel.Item>
-      <Carousel.Item>
-      
+        <p className="projectDescription">{props.dis1}</p>
+          </div></Link>
           
-        <img
-          className="d-block w-100"
-          src={props.img2}
-          alt={props.alt2}
-          height="250px"
-        />
-        <p>{props.dis2}</p>
-      </Carousel.Item>
-      <Carousel.Item>
-     
           
-        <img
-          className="d-block w-100"
-          src={props.img3}
-          alt={props.alt3}
-          height="250px"
-        />
-        <p>
-          {props.dis3}
-          </p>
-        
-         
-        
-      </Carousel.Item>
-    </Carousel>
 
         </Container>
     );
--- src/Components/projects.jsx
+++ src/Components/projects.jsx
@@ -8,6 +8,13 @@ import WM3 from '../assets/project1wm/wm3.png';
 import CTC1 from '../assets/project2ctc/ctc1.png';
 import CTC2 from '../assets/project2ctc/ctc2.png';
 import CTC3 from '../assets/project2ctc/ctc3.png';
+import MSC from '../assets/msc.png';
+import SANASA from '../assets/sanasa.png';
+import POPCORN from '../assets/popcorn.png';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faWebAwesome } from "@fortawesome/free-brands-svg-icons";
+import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+
 
 function Projects(){
     return(
@@ -15,21 +22,36 @@ function Projects(){
             <h3>Projects</h3>
             <Container>
                 <Row>
-                    <Col lg={6}>
-                    <CarouselSlider img1={WM1} Caption="Weather For You" dis1="First ReactJs Project" 
-                    img2={WM2} dis2="Crate using OpenWeatherMap API"
-                    img3={WM3} dis3="Crate using OpenWeatherMap API" />
+                    
+                    <Col lg={4}>
+                    <CarouselSlider Picon={faGithub} project="https://github.com/Kavinda-liyan/Project_1_2022.git" img1={CTC1} Caption="Crystal Tech Computer" 
+                    dis1="Academic Group project (HND) This project is developed using HTML, CSS, JavaScript, and Bootstrap for the frontend, with PHP utilized for the backend. The backend interacts with a SQL database." 
+                     />
                     </Col>
-                    <Col lg={6}>
-                    <CarouselSlider img1={CTC1} Caption="Crystal Tech Computer" dis1="Academic Group project (HND)" 
-                    img2={CTC2} dis2="Project Front-end created by using html,css,js and bootstrap"
-                    img3={CTC3} dis3="Backend develped by using PHP and use sql as database" />
+                    <Col lg={4}>
+                    <CarouselSlider Picon={faGithub} project="https://github.com/Kavinda-liyan/MSCTechnologies.git" img1={MSC} Caption="MSC Technologies" 
+                    dis1="Academic Individual project (HND) This project is developed using HTML, CSS, JavaScript, and Bootstrap for the frontend, with PHP utilized for the backend.
+                     The backend interacts with a SQL database." />
                     </Col>
+                    <Col lg={4}>
+                    <CarouselSlider Picon={faGithub} project="https://github.com/Kavinda-liyan/test-wheather.git" img1={WM1} Caption="Weather For You"
+                     dis1="This project is built using ReactJS for the frontend, with Bootstrap and CSS for styling
+                     . It integrates the OpenWeatherMap API for weather data retrieval." 
+                     />
+                    </Col>
+                    
                     </Row>
                     <Row>
                     <Col lg={4}>
-                    <CarouselSlider/>
+                    <CarouselSlider Picon={faGithub} project="https://github.com/Kavinda-liyan/SanasaGodigamuwa_Project.git" img1={SANASA} Caption="Sanasa Godigamuwa Branch Portfolio" 
+                    dis1="This is a simple static website developed using HTML, CSS, and Bootstrap. It serves as a portfolio for the Sanasa Godigamuwa Branch." />
                     </Col>
+                    <Col lg={4}>
+                    <CarouselSlider Picon={faGlobe} project="https://popcorn.lk/" img1={ POPCORN} Caption="Popcorn Cinema" 
+                    dis1="This is our inaugural collaborative website created using WordPress.
+                     It represents the combined efforts of our team and showcases our collective skills in web development and content management using the WordPress platform." />
+                    </Col>
+                    
                     </Row>
                    
                 
